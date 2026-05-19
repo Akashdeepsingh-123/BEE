a82cf5908536b40f7a4d6e60779836b64d9389de
Akasdeep Singh <akashdeep0904.becse24@chitkara.edu.in>
2026-05-19 18:14:37 +0530

    Added bcrypt password hashing

--- vite.config.js
+++ vite.config.js
@@ -22,10 +22,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, '.')
 
     }
+    
   },
   server: {
     port: 5173,
-    
+
     proxy: {
       '/api': {
         target: 'http://localhost:5120',

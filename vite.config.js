3bedd9ff3a0d5fe684330e2ee49c60fe3d8d9533
Akasdeep Singh <akashdeep0904.becse24@chitkara.edu.in>
2026-05-19 18:14:55 +0530

    Added middleware for token verification

--- vite.config.js
+++ vite.config.js
@@ -26,10 +26,11 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    
+
 
     proxy: {
       '/api': {
+        
         target: 'http://localhost:5120',
         changeOrigin: true
       },
